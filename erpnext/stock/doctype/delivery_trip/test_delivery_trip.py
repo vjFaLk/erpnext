c302688ea64333e91b7217076572da7ce067ffba
Rohan Bansal <rohan@digithinkit.com>
2018-09-05 16:31:55 +0530

    Add Delivery Settings + improve 'Notify Customers' functionality

--- erpnext/stock/doctype/delivery_trip/test_delivery_trip.py
+++ erpnext/stock/doctype/delivery_trip/test_delivery_trip.py
@@ -16,54 +16,57 @@ class TestDeliveryTrip(unittest.TestCase):
 	def setUp(self):
 		create_driver()
 		create_vehicle()
-		create_delivery_notfication()
+		create_delivery_notification()
 		create_test_contact_and_address()
 
 	def test_delivery_trip(self):
 		contact = get_contact_and_address("_Test Customer")
 
 		if not frappe.db.exists("Delivery Trip", "TOUR-00000"):
-			delivery_trip = frappe.get_doc({
-				"doctype": "Delivery Trip",
-				"company": erpnext.get_default_company(),
-				"date": add_days(nowdate(), 5),
-				"driver": "DRIVER-00001",
-				"vehicle": "JB 007",
-				"delivery_notification": "Delivery Notification",
-				"delivery_stops": [{
-					"customer": "_Test Customer",
-					"address": contact.shipping_address.parent,
-					"contact": contact.contact_person.parent
-				}]
+			delivery_trip = frappe.new_doc("Delivery Trip")
+			delivery_trip.company = erpnext.get_default_company()
+			delivery_trip.date = add_days(nowdate(), 5)
+			delivery_trip.driver = "DRIVER-00001"
+			delivery_trip.vehicle = "JB 007"
+
+			delivery_trip.append("delivery_stops", {
+				"customer": "_Test Customer",
+				"address": contact.shipping_address.parent,
+				"contact": contact.contact_person.parent
 			})
+
 			delivery_trip.insert()
+			notify_customers(delivery_trip=delivery_trip.name)
 
-			sender_email = frappe.db.get_value("User", frappe.session.user, "email")
-			notify_customers(docname=delivery_trip.name, date=delivery_trip.date, driver=delivery_trip.driver,
-							vehicle=delivery_trip.vehicle,
-							sender_email=sender_email, delivery_notification=delivery_trip.delivery_notification)
+			self.assertEqual(delivery_trip.email_notification_sent, 1)
 
-			self.assertEqual(delivery_trip.get("delivery_stops")[0].notified_by_email, 0)
 
 
 def create_driver():
 	if not frappe.db.exists("Driver", "Newton Scmander"):
-		driver = frappe.new_doc("Driver")
-		driver.full_name = "Newton Scmander"
-		driver.cell_number = "98343424242"
-		driver.license_number = "B809"
+		driver = frappe.get_doc({
+			"doctype": "Driver",
+			"full_name": "Newton Scmander",
+			"cell_number": "98343424242",
+			"license_number": "B809"
+		})
 		driver.insert()
 
 
-def create_delivery_notfication():
+def create_delivery_notification():
 	if not frappe.db.exists("Email Template", "Delivery Notification"):
-		frappe.get_doc({
+		dispatch_template = frappe.get_doc({
 			'doctype': 'Email Template',
 			'name': 'Delivery Notification',
 			'response': 'Test Delivery Trip',
 			'subject': 'Test Subject',
 			'owner': frappe.session.user
-		}).insert()
+		})
+		dispatch_template.insert()
+
+	delivery_settings = frappe.get_single("Delivery Settings")
+	delivery_settings.dispatch_template = 'Delivery Notification'
+
 
 
 def create_vehicle():
