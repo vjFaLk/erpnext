32f5d5362409b6b5ce1d1ae4633a51ee26058be7
Rohan Bansal <rohan@digithinkit.com>
2018-09-20 16:28:18 +0530

    Fix sum error and empty DN update

--- erpnext/stock/doctype/delivery_trip/delivery_trip.py
+++ erpnext/stock/doctype/delivery_trip/delivery_trip.py
@@ -32,6 +32,9 @@ class DeliveryTrip(Document):
 
 		delivery_notes = list(set([stop.delivery_note for stop in self.delivery_stops if stop.delivery_note]))
 
+		if not delivery_notes:
+			return
+
 		update_fields = {
 			"transporter": self.driver,
 			"transporter_name": self.driver_name,
