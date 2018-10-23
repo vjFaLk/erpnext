74730c4f981101bb9044fd480f9fde10a8eef014
Rohan Bansal <rohan@digithinkit.com>
2018-10-23 17:57:39 +0530

    fix(driver_trips): Add validations for multiple Delivery Trips for the same driver and Delivery Note

--- erpnext/stock/doctype/delivery_trip/delivery_trip.py
+++ erpnext/stock/doctype/delivery_trip/delivery_trip.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 
 import datetime
+import json
 
 import frappe
 from frappe import _
@@ -25,6 +26,7 @@ class DeliveryTrip(Document):
 
 	def validate(self):
 		self.validate_stop_addresses()
+		self.update_status()
 
 	def on_submit(self):
 		self.update_status()
@@ -347,6 +349,21 @@ def get_directions(route, optimize):
 		frappe.throw(_(e.message))
 
 	return directions[0] if directions else False
+
+@frappe.whitelist()
+def validate_unique_delivery_notes(delivery_stops):
+	delivery_stops = json.loads(delivery_stops)
+	delivery_notes = [stop["delivery_note"] for stop in delivery_stops if stop["delivery_note"]]
+
+	if not delivery_notes:
+		return []
+
+	existing_trips = frappe.get_all("Delivery Stop",
+									filters={"delivery_note": ["IN", delivery_notes]},
+									fields=["distinct(parent)"])
+	existing_trips = [stop.parent for stop in existing_trips]
+
+	return existing_trips
 
 
 @frappe.whitelist()
