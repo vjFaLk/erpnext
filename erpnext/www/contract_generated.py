81518d39f5194161d14d69de612937f0b65adeb1
DeeJay <deejay@Indranis-iMac.local>
2019-04-09 18:46:42 +0530

    expiring the unique links to contract

--- erpnext/www/contract_generated.py
+++ erpnext/www/contract_generated.py
@@ -1,18 +1,10 @@
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import cstr, datetime, add_days, now_datetime
+from frappe.utils import cstr, datetime, add_days, now_datetime, time_diff_in_hours, add_to_date
 
 no_cache = 1
 
 def get_context(context):
-    # TODO : Get Token Value from Request Object
-    print("\n======++++++++++++====frappe.local.request", frappe.local.request)
-    print("\n======++++++++++++====frappe.local.request", frappe.local.request.__dict__)
-    print("\n======++++++++++++====type(frappe.local.request.__dict__)", type(frappe.local.request.__dict__))
-
-    print("header", frappe.local.request.headers)
-    print("frappe.local.request.environ", frappe.local.request.environ.get("QUERY_STRING"), "\ntype of query string", type(frappe.local.request.environ.get("QUERY_STRING")))
-
     a = frappe.local.request.environ.get("QUERY_STRING")
     requesting_token = ""
     count = 0
@@ -22,15 +14,15 @@ def get_context(context):
             continue
         elif count == 1:
             requesting_token += cstr(i)
-    print("cstr(requesting_token)", cstr(requesting_token), "type(re", type(requesting_token))     
+   
     token = cstr(requesting_token)
-
-    # TODO : Get Contract based on token in Contract
     doc = frappe.get_doc("Contract", {"hash" : requesting_token})
-    # doc = frappe.get_all('Contract', filters={'hash': requesting_token})[0]
-    print("***doc****", doc)
-    if add_days(doc.hash_generated_on, 1) < now_datetime():
-        context.error = "Invalid link"
+    
+    if now_datetime() > add_to_date(doc.hash_generated_on, hours=24):
+        raise frappe.DoesNotExistError
+        # context.error = "Invalid link"
+          
     else:
         context.contract = doc
-    # TODO : Display relevant Contract Info
+        
+ 
